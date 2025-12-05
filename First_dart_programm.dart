ff3fd4a7f151470843439b1f85e3c7e9e359aefd
Nikodemo Bunkwimba <luhondojinasa@gmail.com>
2025-12-05 11:40:07 +0300

    Variables declaration and pringing outputs

--- First_dart_programm.dart
+++ First_dart_programm.dart
@@ -1,3 +1,13 @@
 void main() {
   print('Hello, Mwl.luhondo');
+
+  //............... Variables declarations.............//
+  int age = 25;
+  String name = "Mwl.luhondo";
+  bool isStudent = false;
+
+  //...................Printing those variables...............//
+  print(age);
+  print(name);
+  print(isStudent);
 }
